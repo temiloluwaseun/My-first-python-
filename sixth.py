6af8941a6fa680a6a1d97068b92ba60901f447d5
temiloluwaseun <113363888+temiloluwaseun@users.noreply.github.com>
2022-09-15 14:57:04 +0100

    Add files via upload

---
+++ sixth.py
@@ -0,0 +1,13 @@
+schoolsinabk= {
+    "Kindom heritage":49,
+    "Patterson":57,
+    "Babcock": 29,
+    "Baptist girls":125,
+    "Abeo":27,
+    "Victorotti":93,
+    "Roseborn":28,
+}
+schoolsinabk2= ["kindom heritage","patterson","babcock","baptist girls","abeo","victorotti","roseborn"]
+
+print(schoolsinabk)
+print(schoolsinabk2)
