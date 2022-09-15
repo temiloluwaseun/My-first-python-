8b92712ccc3a71fed25f723be3c1159d103297f7
temiloluwaseun <113363888+temiloluwaseun@users.noreply.github.com>
2022-09-15 15:44:45 +0100

    Add files via upload

---
+++ eight.py
@@ -0,0 +1,35 @@
+box= 1.8
+box2=[6,4,9,2]
+box3=[8,6,2,4]
+box4= 7
+box5=False
+
+print(type(box))
+print(type(box2))
+print(type(box3))
+print(type(box4))
+print(type(box5))
+
+print(box)
+print(box2)
+print(box3)
+print(box4)
+print(box5)
+
+newbox= int(box)
+newbox2= list(box2)
+newbox3= set(box3)
+newbox4= str(box4)
+newbox5= str(box5)
+
+print(type(newbox))
+print(type(newbox2))
+print(type(newbox3))
+print(type(newbox4))
+print(type(newbox5))
+
+print(newbox)
+print(newbox2)
+print(newbox3)
+print(newbox4)
+print(newbox5)
